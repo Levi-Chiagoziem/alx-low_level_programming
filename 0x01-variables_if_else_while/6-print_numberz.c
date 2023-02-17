73552968236bea65c02e88e960df129cd66219c0
Levi <Levichiagoziem@gmail.com>
2023-02-17 06:50:16 -0800

    task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ **/
+int main(void)
+{
+	int hexa;
+
+	for (hexa = 0; hexa < 10; hexa++)
+	{
+		putchar(hexa);
+	}
+	putchar('\n');
+	return (0);
+}
