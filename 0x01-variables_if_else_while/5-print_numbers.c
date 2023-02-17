de44945aef1bad38ae54ec1072ef9edf60ccf2cd
Levi <Levichiagoziem@gmail.com>
2023-02-17 06:16:00 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ **/
+int main(void)
+{
+	int hex;
+
+	for (hex = 0; hex < 10; hex++)
+	{
+		printf("%x\n", hex);
+	}
+	return (0);
+}
