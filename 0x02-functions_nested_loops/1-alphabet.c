448ece28fd3afd63beeefef64d112897d3cda8d9
Levi <Levichiagoziem@gmail.com>
2023-02-21 04:52:15 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		_putchar(low);
+	}
+	_putchar('\n');
+	return (0);
+}
