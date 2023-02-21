09767787e8a72e87f83793ab1ea17c9d5da996b3
Levi <Levichiagoziem@gmail.com>
2023-02-21 05:24:19 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		char alph = 'a'
+			while (alph <= 'z')
+			{
+				_putchar(alph);
+			}
+		_putchar('\n');
+	}
+}
