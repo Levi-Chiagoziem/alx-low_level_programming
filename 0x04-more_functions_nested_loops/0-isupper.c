89b87f0e3d0ae347c4b82d2e320ded9ba1e3d0aa
Levi <Levichiagoziem@gmail.com>
2023-02-23 04:00:52 -0800

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _isupper - function
- * @c:Tested character
+ * _isupper - function that verifies if c is upper or not
+ * @c: tested character
  * Return: returns 1 if c is uppercase and returs 0 of c is lower case
  */
 int _isupper(int c)
