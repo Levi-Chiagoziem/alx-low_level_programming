9c54e7096acb0abdd028cdc50164fcc3a8f38856
Levi <Levichiagoziem@gmail.com>
2023-02-23 06:41:17 -0800

    task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,7 +13,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char);
+int _putchar(int);
 void print_number(int n);
 
 #endif /* MAIN_H */
