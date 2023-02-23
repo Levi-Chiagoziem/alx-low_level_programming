db0ad65b151c9f9d83b5e6e940778613795e6584
Levi <Levichiagoziem@gmail.com>
2023-02-23 05:50:45 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function
+ *
+ * Return: The value of code
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	return ('\n');
+}
